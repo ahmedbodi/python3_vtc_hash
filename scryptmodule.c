745bf6de0a80fc904b9615d91ffe16092e026edf
root <root@eloi-btc.MulticoinCloud.d4.internal.cloudapp.net>
2014-08-06 20:22:59 -0400

    Python 3 Compatability

--- scryptmodule.c
+++ scryptmodule.c
@@ -40,19 +40,31 @@ static PyObject *scrypt_getpowhash(PyObject *self, PyObject *args)
     char *output;
     unsigned char Nfactor;
     PyObject *value;
+#if PY_MAJOR_VERSION >= 3
+    PyBytesObject *input;
+#else
     PyStringObject *input;
+#endif
     if (!PyArg_ParseTuple(args, "S", &input))
         return NULL;
-    
+
     Py_INCREF(input);
     output = PyMem_Malloc(32);
-    
+
+#if PY_MAJOR_VERSION >= 3
+    char *blockheader = (char *)PyBytes_AsString((PyObject*) input);
+#else
     char *blockheader = (char *)PyString_AsString((PyObject*) input);
-    
+#endif
+
     Nfactor = getNfactor(blockheader);
     scrypt_N_sse2(blockheader, output, Nfactor);
     Py_DECREF(input);
+#if PY_MAJOR_VERSION >= 3
+    value = Py_BuildValue("y#", output, 32);
+#else
     value = Py_BuildValue("s#", output, 32);
+#endif
     PyMem_Free(output);
     return value;
 }
@@ -62,6 +74,22 @@ static PyMethodDef ScryptMethods[] = {
     { NULL, NULL, 0, NULL }
 };
 
+#if PY_MAJOR_VERSION >= 3
+static struct PyModuleDef ScryptModule = {
+    PyModuleDef_HEAD_INIT,
+    "vtc_scrypt",
+    "...",
+    -1,
+    ScryptMethods
+};
+
+PyMODINIT_FUNC PyInit_vtc_scrypt(void) {
+    return PyModule_Create(&ScryptModule);
+}
+
+#else
+
 PyMODINIT_FUNC initvtc_scrypt(void) {
     (void) Py_InitModule("vtc_scrypt", ScryptMethods);
 }
+#endif
